eb80b200e1dcf5f6d38b91fcf8e2af3e01577759
dylan_jacquot <Dylan.Jacquot.Etu@univ-lemans.fr>
2022-06-15 19:18:58 +0200

    V0.8.0 Début de la conception du RecyclerView des propositions

--- app/src/main/java/com/example/democratisapp/ui/commissions/CommissionsFragment.kt
+++ app/src/main/java/com/example/democratisapp/ui/commissions/CommissionsFragment.kt
@@ -5,15 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.democratis.classes.Commission
-import com.example.democratisapp.MainActivity
 import com.example.democratisapp.database.DemocratisDB
 import com.example.democratisapp.databinding.FragmentCommissionsBinding
-import com.example.democratisapp.ui.login.LoginActivity
 import com.example.kotlindeezer.ui.RecyclerCommissionAdapter
 
 class CommissionsFragment : Fragment() {
